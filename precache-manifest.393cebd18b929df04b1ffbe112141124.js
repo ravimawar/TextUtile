6b5c5ab39115e653e6f7ca40e8ce309bc08fe9db
Rv Mawar <Rvmawar99@gmail.com>
2022-07-28 18:04:02 +0530

    Updates

---
+++ precache-manifest.393cebd18b929df04b1ffbe112141124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "86eb6d63597a558d09d4",
+    "url": "/TextUtile/static/css/main.2bd5beab.chunk.css"
+  },
+  {
+    "revision": "86eb6d63597a558d09d4",
+    "url": "/TextUtile/static/js/main.86eb6d63.chunk.js"
+  },
+  {
+    "revision": "2998caf54286a93ccb62",
+    "url": "/TextUtile/static/js/1.2998caf5.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtile/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "ba3ea859fe943324fddc",
+    "url": "/TextUtile/static/js/runtime~main.ba3ea859.js"
+  },
+  {
+    "revision": "5e8b8ba4f2623968c0d7f085ceed5ac7",
+    "url": "/TextUtile/index.html"
+  }
+];
